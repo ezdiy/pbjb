9e85a93f73bde5368137bf10dea4b37b0ad5bad7
ezdiy <ezzzdiy@gmail.com>
2019-11-30 09:08:00 +0100

    Fix smbpasswd typo

--- svc/init.d/00-setpass.sh
+++ svc/init.d/00-setpass.sh
@@ -7,7 +7,7 @@ if [ ! -e /mnt/secure/etc/passwd ] || [ -n "$(find -L /mnt/ext1/password.txt -pr
 fi
 suff=":[U          ]:LCT-00000001:"
 pw="$(cat /mnt/secure/etc/passwd)"
-(echo "root:0:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"$(ntlmhash "$pw")"$suff"; echo "reader:100:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"$(ntlmhash "")"$suff") > /mnt/secure/etc/samba/
+(echo "root:0:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"$(ntlmhash "$pw")"$suff"; echo "reader:100:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"$(ntlmhash "")"$suff") > /mnt/secure/etc/samba/smbpasswd
 
 echo -e "ftp:*:100:100::/mnt/ext1/public:/bin/false\nroot:"$(mkpasswd "$pw")":0:0::/mnt/ext1:/bin/false" > /mnt/secure/etc/ftpd.passwd
 chmod 700 /mnt/secure/etc/ftpd.passwd
