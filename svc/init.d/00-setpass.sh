f3b5938d423adcd1dc6bc850c3b92686072da879
ezdiy <ezzzdiy@gmail.com>
2019-12-02 00:38:16 +0100

    Fix password handling.

--- svc/init.d/00-setpass.sh
+++ svc/init.d/00-setpass.sh
@@ -1,13 +1,16 @@
 #!/mnt/secure/su /bin/sh
-export TZ=
-nopw="password=(keep unchanged)"
-if [ ! -e /mnt/secure/etc/passwd ] || [ -n "$(find -L /mnt/ext1/rootpassword.txt -prune -newer /mnt/secure/etc/passwd)" ]; then
-        if [ ! -e /mnt/ext1/rootpassword.txt ] || [ "$(cat /mnt/ext1/rootpassword.txt)" == "$nopw" ]; then
-                echo "password=$RANDOM" > /mnt/ext1/rootpassword.txt
-        fi
-	. /mnt/ext1/rootpassword.txt
-	echo -n $password > /mnt/secure/etc/passwd
+if [ -e /mnt/ext1/rootpassword.txt ]; then
+	password="$(cut -c 10- < /mnt/ext1/rootpassword.txt)"
+	if [ "$password" == "(keep unchanged)" ]; then
+		if [ -e /mnt/secure/etc/passwd ]; then
+			exit 0
+		fi
+		password=$RANDOM
+		echo "password=$password" > /mnt/ext1/rootpassword.txt
+	fi
+	echo -n "$password" > /mnt/secure/etc/passwd
 fi
+
 suff=":[U          ]:LCT-00000001:"
 pw="$(cat /mnt/secure/etc/passwd)"
 (echo "root:0:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"$(ntlmhash "$pw")"$suff"; echo "reader:100:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"$(ntlmhash "")"$suff") > /mnt/secure/etc/samba/smbpasswd
