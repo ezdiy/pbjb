05f3e12219aa2623e00cac6a0912d4bf5dc22a6b
ezdiy <ezzzdiy@gmail.com>
2019-12-05 01:22:12 +0100

    try fix SD card mounting as mass storage.

--- svc/bin/ins_usb_mod.sh
+++ svc/bin/ins_usb_mod.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 DIR=/sys/class/android_usb/android0
 echo "/dev/mmcblk0p1" > ${DIR}/f_mass_storage/lun/file
+if grep -q mmcblk1p1 /proc/partitions; then
+	echo mmcblk1p1 > ${DIR}/f_mass_storage/lun1/file
+fi
+if grep -q mmcblk2p1 /proc/partitions; then
+	echo mmcblk2p1 > ${DIR}/f_mass_storage/lun2/file
+fi
+
--- svc/bin/rm_usb_mod.sh
+++ svc/bin/rm_usb_mod.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 DIR=/sys/class/android_usb/android0
 echo > ${DIR}/f_mass_storage/lun/file
+echo > ${DIR}/f_mass_storage/lun1/file
+echo > ${DIR}/f_mass_storage/lun2/file
