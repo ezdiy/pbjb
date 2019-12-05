69594f2a307bb21add01a983950d4f89c0dfbeae
ezdiy <ezzzdiy@gmail.com>
2019-12-05 01:39:32 +0100

    SD fix2

--- svc/bin/ins_usb_mod.sh
+++ svc/bin/ins_usb_mod.sh
@@ -2,9 +2,9 @@
 DIR=/sys/class/android_usb/android0
 echo "/dev/mmcblk0p1" > ${DIR}/f_mass_storage/lun/file
 if grep -q mmcblk1p1 /proc/partitions; then
-	echo mmcblk1p1 > ${DIR}/f_mass_storage/lun1/file
+	echo /dev/mmcblk1p1 > ${DIR}/f_mass_storage/lun1/file
 fi
 if grep -q mmcblk2p1 /proc/partitions; then
-	echo mmcblk2p1 > ${DIR}/f_mass_storage/lun2/file
+	echo /dev/mmcblk2p1 > ${DIR}/f_mass_storage/lun2/file
 fi
 
