2fd117a61322d4e4de1c30f20ef672f4b9a6411c
ezdiy <ezdiy@outlook.com>
2019-12-08 16:48:56 +0100

    add support for service restart after usb storage release

--- svc/bin/rm_usb_mod.sh
+++ svc/bin/rm_usb_mod.sh
@@ -3,3 +3,9 @@ DIR=/sys/class/android_usb/android0
 echo > ${DIR}/f_mass_storage/lun/file
 echo > ${DIR}/f_mass_storage/lun1/file
 echo > ${DIR}/f_mass_storage/lun2/file
+
+# Resume services that request it
+export PATH=/mnt/secure/bin:/sbin:/usr/sbin:$PATH
+for f in /tmp/resume.*; do
+	"$f"
+done
--- svc/init.d/40-httpd.sh
+++ svc/init.d/40-httpd.sh
@@ -1,4 +1,5 @@
 #!/mnt/secure/su /bin/sh
 ##HTTP server
 mkdir /mnt/ext1/public_html
+ln -s $0 /tmp/service.$1
 lighttpd -f /mnt/secure/etc/lighttpd.conf
