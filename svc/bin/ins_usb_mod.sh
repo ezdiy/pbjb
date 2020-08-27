fe53ca2236b8031e8eba96c9b64674288bfc4fe2
Ez Diy <ezdiy@outlook.com>
2020-08-27 22:19:57 +0200

    Use partition names for storage.

--- svc/bin/ins_usb_mod.sh
+++ svc/bin/ins_usb_mod.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 DIR=/sys/class/android_usb/android0
-echo "/dev/mmcblk0p1" > ${DIR}/f_mass_storage/lun/file
-if grep -q mmcblk1p1 /proc/partitions; then
-	echo /dev/mmcblk1p1 > ${DIR}/f_mass_storage/lun1/file
-fi
-if grep -q mmcblk2p1 /proc/partitions; then
-	echo /dev/mmcblk2p1 > ${DIR}/f_mass_storage/lun2/file
-fi
-
+echo "/dev/user_int" > ${DIR}/f_mass_storage/lun/file
+echo "/dev/user_ext" > ${DIR}/f_mass_storage/lun/file
