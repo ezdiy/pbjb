c3ddeaff1e8beca573ba6b4b1a7f86e18d459279
Ez Diy <ezdiy@outlook.com>
2020-09-19 15:33:14 +0200

    Fix lun name for external storage.

--- svc/bin/ins_usb_mod.sh
+++ svc/bin/ins_usb_mod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DIR=/sys/class/android_usb/android0
 echo "/dev/user_int" > ${DIR}/f_mass_storage/lun/file
-echo "/dev/user_ext" > ${DIR}/f_mass_storage/lun/file
+echo "/dev/user_ext" > ${DIR}/f_mass_storage/lun1/file
